6d6990837fafdcf9bb857e3d6f29ebcad1c16873
veerachot <veerachot.ro.64@ubu.ac.th>
2024-02-29 16:44:04 +0700

    msg

--- SriKhai_Recycle_Factory/SriKhai_Recycle_Factory/settings.py
+++ SriKhai_Recycle_Factory/SriKhai_Recycle_Factory/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 STATICFILES_DIRS = [
  os.path.join(BASE_DIR, 'static')
  ]
-STATIC_URL =  os.path.join(BASE_DIR, 'static')
-STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL =  os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles/')
 
 
 MEDIA_URL = 'media/'
